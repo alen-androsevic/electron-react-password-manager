cf412aa91668ff76edbbdfb36d9d99fd4c49e767
Michael de Groot <specamps@gmail.com>
2016-05-23 07:17:40 +0200

    rsa passphrase instead of local db sha1 :P

--- main.js
+++ main.js
@@ -7,44 +7,28 @@ const defaultPass = "admin";
 const maintitle = "Password App";
 var nStore = require('nstore');
 nStore = nStore.extend(require('nstore/query')());
-var NodeRSA = require('node-rsa');
+var cryptico = require('cryptico');
 var key;
-var pemKey;
-
-// TODO: find a way to secure the pem ? :P
-fs.readFile('./keys/private.pem', 'utf8', function (err,data) {
-  if (err) throw new Error(err);
-  pemKey = data;
-  key = new NodeRSA(data);  
-});
 
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 const dev = true
  
-var isNew = true;
 var appReady = false;
 
+var myRSA;
+var myPUB;
+ 
 let mainWindow 
 
-try {
-    fs.accessSync("./data.db", fs.F_OK);
-    isNew = false;
-} catch (e) {}
-
 var waitForAppInterval = setInterval(waitForApp,100);
-function waitForApp(){
-  if(appReady){ 
-    if(isNew){
-      mainWindow.loadURL(`file://${__dirname}/firsttime.html`)
-    }else{
-      mainWindow.loadURL(`file://${__dirname}/login.html`)
-    }
+function waitForApp(){ 
+  if(appReady){  
+    mainWindow.loadURL(`file://${__dirname}/login.html`)
     clearInterval(waitForAppInterval)
   } 
 }
   
-var db = nStore.new('./data.db');
 var passwords = nStore.new('./passdb.db'); 
 
 app.on('ready', function(){
@@ -74,68 +58,20 @@ function createApp(){
   appReady = true;
 } 
 
-ipcMain.on('create', (event, data) => {
-  if(data.pass1.length===0){
-    msg = {
-      result: false,
-      humane: {
-        title: maintitle,
-        msg: "Password length too low"
-      }
-    }
-    event.sender.send('reply', msg);
-    return;
-  }
-  if(data.pass1 !== data.pass2){ 
-    msg = {
-      result: false,
-      humane: { 
-        title: maintitle,
-        msg: "Passwords do not match"
-      }
-    }
-    event.sender.send('reply', msg);
-    return;
-  }
-  db.save("mainpass", {password: hashPass(data.pass1), created: Date.now()}, function (err) {
-      if (err) { throw err; }
-      msg = {
-        result: true,
-        humane: {
-          title: maintitle,
-          msg: "Account created!"
-        }
-      }
-      event.sender.send('reply', msg);
-      mainWindow.loadURL(`file://${__dirname}/login.html`)
-  });
-});
-
 ipcMain.on('login', (event, inputPass) => { 
-  db.get("mainpass", function (err, res) {
-   if(err) throw new Error(err)
-    if(passwordHash.verify(inputPass, res.password)){
-      msg = {
-        result: false, 
-        humane: {
-          title: maintitle,
-          msg: "Login succeeded, please wait.." 
-        }
-      }
-      event.sender.send('reply', msg);
-      
-      mainWindow.loadURL(`file://${__dirname}/index.html`)
-    }else{ 
-      msg = {
-        result: true,
-        humane: {
-          title: maintitle,
-          msg: "Login failed, please try again."
-        }
-      }
-      event.sender.send('reply', msg);
+  msg = {
+    result: false, 
+    humane: {
+      title: maintitle,
+      msg: "Login succeeded, please wait.." 
     }
-  });
+  }
+  event.sender.send('reply', msg);
+  
+  myRSA = cryptico.generateRSAKey(inputPass, 2048);
+  myPUB = cryptico.publicKeyString(myRSA);  
+  
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
 });
  
 ipcMain.on('addService', (event, post) => {
@@ -153,38 +89,29 @@ ipcMain.on('addService', (event, post) => {
         event.sender.send('reply', msg);
     });
   });
-});
- 
+});  
+
 ipcMain.on('populateTable',  (event) => {
   passwords.all(function (err, results) {
     if(err) throw new Error(err)
-    for(i in results){
+    for(i in results){ 
       decryptString(results[i].password, function(decrypted){
         results[i].password = decrypted
       });
-    }
+    } 
     event.sender.send('passtable', results);
   });
-}); 
+});   
 
 function encryptString(string,cb){
-  try { 
-    var encrypted = key.encrypt(string, 'base64')
-  } catch(e){
-    console.log(e);
-  } 
-  if(cb) cb(encrypted);
-}
+  var encrypted = cryptico.encrypt(string, myPUB)
+  if(cb) cb(encrypted.cipher);
+} 
 function decryptString(string,cb){
-    key.importKey(pemKey, 'pkcs8'); 
-    try { 
-      var decrypted = key.decrypt(string,'utf8')
-    } catch(e){ 
-      console.log(e);
-    }
-  if(cb) cb(decrypted);
+  var decrypted = cryptico.decrypt(string, myRSA);
+  if(cb) cb(decrypted.plaintext); 
 }
 
-function hashPass(pass){
+function hashPass(pass){ 
   return passwordHash.generate(pass);
 }
