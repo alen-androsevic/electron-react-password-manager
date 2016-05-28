4962a93b598b44c160d285db608c38534b39e0d6
Michael de Groot <specamps@gmail.com>
2016-05-28 21:48:04 +0200

    version 0.0.3 pushing from atom editor test :)

--- README.md
+++ README.md
@@ -10,8 +10,8 @@
 #### > Features
  - Node.js, ES6, Built with [Electron](http://electron.atom.io/)
  - Random salt for PBKD2F that is generated on first run
- - PBKD2F 300k iterations
- - 2048bit RSA passphrase encryption
+ - up to 400k PBKD2F iterations
+ - up to 4096bit RSA passphrase encryption
  - Stores only passwords encrypted, to save time for decryption :)
  - No accounts just a password you come up with to rule them all
  - Your password is your passphrase for your encryption key (don't make it too small, it is salted though)
--- main.js
+++ main.js
@@ -1,34 +1,18 @@
+'use strict'
+
 const electron = require('electron')
-const connection = new(require('nosqlite').Connection)('./inc/db')
 
-electron.crypt = {
-  bits: 2048,
-  supersalt: {
-    saltLen: 2048,
-    maxpos: 190
-  },
-  pbkd2f: {
-    iterations: 300000,
-    count: 420
-  }
-}
-
-electron.dev = false
-electron.ipcMain = electron
-electron.db = {
-  passwords: connection.database('passwords'),
-  salt: connection.database('salt'),
-  encryption: connection.database('encryption')
-}
-
-electron.log = log => {
-  console.log(log) 
-}
+const settings = require('./lib/electron.js').init(electron, (err, data) => {
+  if (err)
+    throw new Error(err)
+})
 
 const events = require('./lib/events').init(electron, (err, data) => {
-  if(err) throw new Error(err)
+  if (err)
+    throw new Error(err)
 })
 
 const socket = require('./lib/socket').init(electron, (err, data) => {
-  if(err) throw new Error(err)
+  if (err)
+    throw new Error(err)
 })
