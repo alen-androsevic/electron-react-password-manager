72267de5171d29418a793ad2ee33483a5436f56c
Michael de Groot <specamps@gmail.com>
2016-06-09 23:00:50 +0200

    style edits

--- src/inc/react/src/index.js
+++ src/inc/react/src/index.js
@@ -12,15 +12,11 @@ const ControlLabel   = require('react-bootstrap').ControlLabel
 const FormControl    = require('react-bootstrap').FormControl
 const Table          = require('react-bootstrap').Table
 
-exports.kappa = () => {
-  console.log("kappa")
-}
-
 // Each table row
 const PasswordRow = React.createClass({
   render: function() {
     // TODO: maak dit af, auto hidden van wachtwoord met sterretjes
-    // const password = "***"
+    // const password = '***'
     return (
       <tr>
         <td>{this.props.password.service}</td>
@@ -29,7 +25,7 @@ const PasswordRow = React.createClass({
         <td></td>
       </tr>
     )
-  }
+  },
 })
 
 // How the table is created
@@ -37,14 +33,16 @@ const TableCreate = React.createClass({
   handleRowRemove: function(password) {
     var index = -1
     var clength = this.props.passwords.length
-		for(var i = 0;i < clength;i++ ) {
-			if(this.props.passwords[i].id === password.id) {
-				index = i
-				break
-			}
-		}
-		this.props.passwords.splice(index, 1)
+    for (var i = 0;i < clength; i++) {
+      if (this.props.passwords[i].id === password.id) {
+        index = i
+        break
+      }
+    }
+
+    this.props.passwords.splice(index, 1)
   },
+
   render: function() {
     var rows = []
     var lastCategory = null
@@ -52,6 +50,7 @@ const TableCreate = React.createClass({
       rows.push(<PasswordRow password={password} key={password.id} />)
       lastCategory = password.category
     })
+
     return (
       <Table striped bordered condensed hover responsive>
         <thead>
@@ -65,7 +64,7 @@ const TableCreate = React.createClass({
         <tbody>{rows}</tbody>
       </Table>
     )
-  }
+  },
 })
 
 // The add password form
@@ -78,17 +77,27 @@ const AddPasswordForm = React.createClass({
       password:  '',
     }
   },
-  handleServiceChange: function(e){
+
+  handleServiceChange: function(e) {
     this.setState({service: e.target.value})
   },
-  handleEmailChange: function(e){
+
+  handleEmailChange: function(e) {
     this.setState({email: e.target.value})
   },
-  handlePasswordChange: function(e){
+
+  handlePasswordChange: function(e) {
     this.setState({password: e.target.value})
   },
+
   handleSubmit: function(e) {
     e.preventDefault()
+
+    if (this.state.service.length === 0 || this.state.email.length === 0 || this.state.password.length === 0) {
+      appNotify('Neither fields can be empty')
+      return
+    }
+
     var post = {
       service:  this.state.service,
       email:    this.state.email,
@@ -96,44 +105,44 @@ const AddPasswordForm = React.createClass({
     }
     ipcRenderer.send('addService', post)
   },
-  render: function (){
+
+  render: function() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <FormGroup controlId="formControlsText">
+        <FormGroup controlId='formControlsText'>
           <ControlLabel>Service</ControlLabel>
-          <FormControl onChange={this.handleServiceChange} value={this.state.service} type="text" placeholder="Example: Steam, Google" />
+          <FormControl onChange={this.handleServiceChange} value={this.state.service} type='text' placeholder='Example: Steam, Google' />
         </FormGroup>
-        <FormGroup controlId="formControlsText">
+        <FormGroup controlId='formControlsText'>
           <ControlLabel>Email address</ControlLabel>
-          <FormControl onChange={this.handleEmailChange} value={this.state.email} type="text" placeholder="Enter email/username" />
+          <FormControl onChange={this.handleEmailChange} value={this.state.email} type='text' placeholder='Enter email/username' />
         </FormGroup>
-        <FormGroup controlId="formControlsPassword">
+        <FormGroup controlId='formControlsPassword'>
           <ControlLabel>Password</ControlLabel>
-          <FormControl onChange={this.handlePasswordChange} value={this.state.password} type="password" />
+          <FormControl onChange={this.handlePasswordChange} value={this.state.password} type='password' />
         </FormGroup>
-        <Button type="submit">
+        <Button type='submit'>
           Add Password
         </Button>
       </form>
     )
-  }
+  },
 })
 
 // The add password button
 const AddPasswordButton = React.createClass({
-  render: function (){
+  render: function() {
     return (
       <div>
-        <Button onClick={this.open} type="submit">
+        <Button onClick={this.open} type='submit'>
           Add Password
         </Button>
 
-        <Modal id="createPasswordModal" show={this.state.showModal} onHide={this.close}>
+        <Modal id='createPasswordModal' show={this.state.showModal} onHide={this.close}>
           <Modal.Header>
             <Modal.Title>Add a service</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <h4>Add a Password</h4>
             <AddPasswordForm/>
           </Modal.Body>
           <Modal.Footer>
@@ -168,7 +177,7 @@ const Main = React.createClass({
         <TableCreate passwords={this.props.passwords} />
       </div>
     )
-  }
+  },
 })
 
 // When we receive server data
